e82abb350f521fdf749657ddc493dff1c6436ac4
eflo-ria <loelia.christen@liip.ch>
2024-11-09 23:21:45 +0100

    Update +page.svelte
    update fro switzerland

--- src/routes/countries/learn/switzerland/+page.svelte
+++ src/routes/countries/learn/switzerland/+page.svelte
@@ -1,1 +1,12 @@
 <!-- Write or paste an article with HTML & Svelte here -->
+<script>
+</script>
+
+<h1 class="text-4xl font-bold mb-4">Switzerland</h1>
+<p class="mb-2">The actual languages spoken in Switzerland are : </p>
+<ul class="mb-2 list-disc list-inside">
+<li>Swiss-German</li>
+<li>French</li>
+<li>Italian</li>
+<li>Romansh</li>
+</ul>
